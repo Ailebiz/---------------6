e7d3794c615c8267cdeb206af5ef03dd5c47ceee
Ailebiz <bolkyzyaliza@gmail.com>
2024-12-09 17:49:32 +0500

    /

---
+++ js/exam.js
@@ -0,0 +1,97 @@
+let changeschool=[
+         
+             {
+                name:"Toqotaev",
+                phonenumber: 877537491290,
+          
+             },
+         
+ 
+           
+        students =[
+         { studentname:"Ayaulym",
+         top:"A-top",
+         progress:97,
+         lessons:2,},
+
+         {studentname:"Aqdidar",
+         top:"B-top",
+         progress:85,
+         lessons:3},
+
+         {studentname:"Aqdidar",
+            top:"C-top",
+            progress:77,
+            lessons:1},
+        ],
+
+       
+    
+]
+
+// #2
+
+function changsechoolname () {
+     const newnum = prompt("Mektepke atau oilanyz" )
+      if (changsechoolname.newnum) {
+         this.newnum.push
+         console.log(changeschool);
+            
+      } else {
+    console.log("Qate engizdiniz");   
+}
+
+}
+
+//#3
+ let changePhoneNumber= +prompt("Zhana nomer zhazynyz")
+
+  function changePhoneNumber(){
+        if (changeschool.phonenumber) {
+            this.changePhoneNumber.push
+            console.log(changePhoneNumber);
+            
+        } else {
+            "Qate engizdiniz"
+        }
+  }
+
+//#4
+  
+
+    console.log(changeschool.students.name);
+
+
+ 
+// #5
+function addstydent(Zhana) {
+    let esim= prompt("Oqushy aty")
+    const ulgerim = +prompt("Oqushy ulgerimi")
+    const top = prompt("Oqushy toby")
+
+}  
+
+//6
+let tan=+prompt("1 den 6ga deiin san tandanyz")
+if (tan = 1) {
+    console.log();
+    
+}else if(tan=2){
+      console.log();
+      
+}else if(tan=3){
+    console.log();
+    
+}else if(tan=4){
+    console.log();
+    
+}else if(tan=5){
+    console.log();
+    
+}else if(tan=6){
+    console.log();
+    
+}
+
+ 
+
